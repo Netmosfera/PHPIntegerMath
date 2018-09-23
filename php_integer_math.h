a6d4ab9118d918c9398b52bd4012f8f5b053c391
Netmosfera <netmosfera@gmail.com>
2018-09-23 21:34:32 +0200

    Support PHP 7.2

--- php_integer_math.h
+++ php_integer_math.h
@@ -4,7 +4,7 @@
     extern zend_module_entry integer_math_module_entry;
     #define phpext_integer_math_ptr &integer_math_module_entry
 
-    #define PHP_INTEGER_MATH_VERSION "7.3"
+    #define PHP_INTEGER_MATH_VERSION "7.2"
     #define PHP_INTEGER_MATH_EXTNAME "Integer Math"
 
     #ifdef ZTS
